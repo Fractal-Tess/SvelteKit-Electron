1255fabb46f8410f7aff3ea4f6a96398e103820f
Fractal-Tess <vgfractal@gmail.com>
2022-09-20 22:42:45 +0300

    fixed style

--- src/lib/example/versions/Versions.svelte
+++ src/lib/example/versions/Versions.svelte
@@ -2,7 +2,7 @@
   const versions = Object.entries(api.versions.all());
 </script>
 
-<ul class="grid grid-cols-2 grid- ">
+<ul class="grid grid-cols-2 md:grid-cols-3">
   {#each versions as [key, val]}
     <li class="mx-14">{key} - {val}</li>
   {/each}
