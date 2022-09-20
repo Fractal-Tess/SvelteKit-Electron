f91fa0ca9de1b705398da2cadfa91e7e7d690139
Fractal-Tess <vgfractal@gmail.com>
2022-09-20 06:00:31 +0300

    standard theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,28 +21,10 @@ const config = {
     themes: [
       {
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-          primary: '#e863d6',
-          secondary: '#7bc3e0',
-          accent: '#cd97fc',
-          neutral: '#25343C',
-          'base-100': '#F8F8FB',
-          info: '#93BADC',
-          success: '#12A168',
-          warning: '#F4C857',
-          error: '#DE3B4E'
+          ...require('daisyui/src/colors/themes')['[data-theme=light]']
         },
         dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-          primary: '#137cb5',
-          secondary: '#e80d52',
-          accent: '#f4cd73',
-          neutral: '#1F1721',
-          'base-100': '#2B3440',
-          info: '#35B0F3',
-          success: '#1B743C',
-          warning: '#F38A12',
-          error: '#F2215C'
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]']
         }
       }
     ]
